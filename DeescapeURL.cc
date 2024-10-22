1daf4bb2acff69a173bbfea82b24646f126ca222
Aaron <lantianye@163.com>
2024-10-22 16:35:02 +0800

    Update DeescapeURL.cc
    update

--- DeescapeURL.cc
+++ DeescapeURL.cc
@@ -1,3 +1,6 @@
+#include <string>
+using namespace std;
+
 short int hexChar2dec(char c) {
   if ( '0'<=c && c<='9' ) {
     return short(c-'0');
@@ -17,11 +20,21 @@ string DeescapeURL(const string &URL) {
     if ( c != '%' ) {
       result += c;
     } else {
+      // 检查是否超出字符串边界
+      if (i + 2 >= URL.size()) {
+        // 可以记录错误或者跳过转义序列
+        continue; // 或者 break; 或者其他错误处理逻辑
+      }
       char c1 = URL[++i];
       char c0 = URL[++i];
-      int num = 0;
-      num += hexChar2dec(c1) * 16 + hexChar2dec(c0);
-      result += char(num);
+      int high = hexChar2dec(c1);
+      int low = hexChar2dec(c0);
+      // 检查是否是有效的十六进制转义字符
+      if (high != -1 && low != -1) {
+        result += char(high * 16 + low);
+      } else {
+        result += c;
+      }
     }
   }
   return result;
